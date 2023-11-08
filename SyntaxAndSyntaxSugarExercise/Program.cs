f2122aca2c997b6e1ffd6fed28a9aefd28621430
Micah Ammons <micahammons@gmail.com>
2023-11-08 11:50:26 -0500

    Syntax Practice

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Inferred typing
+            var answer = 4;
+            var response = "";
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+
+            //string interpolation 
+
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = $"{answer} is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+
+            //Ternary Operator
+
+            int answer = 4;
+            string response = (answer < 9) ? $"{answer} is less than nine." : $"Answer is greater than nine";
+            Console.WriteLine(response);
+
         }
     }
 }
